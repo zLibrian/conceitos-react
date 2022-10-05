c373794045390cbf1931bc2dee0692cf2fd2bd53
Kevin Willian <kevinwillian084@gmail.com>
2022-10-05 16:21:26 -0300

    feat: adiciona funcionalidades de manipulacao de task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,18 +15,24 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+    const id = tasks[tasks.length - 1]?.id + 1 || 1;
+    const newTask = {id, title: newTaskTitle, isComplete: false};
+    setTasks(prevState => [...prevState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(prevState => prevState.map(task => (
+      task.id === id ? {...task, isComplete: !task.isComplete} : task))
+    );
   }
-
+  
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(prevState => prevState.filter(task => task.id !== id));
   }
 
-  return (
+  return (  
     <section className="task-list container">
       <header>
         <h2>Minhas tasks</h2>
