152b184549b3d3f730cc71108baa1487bc3ce6a5
Kevin Willian <kevinwillian084@gmail.com>
2022-10-12 16:53:24 -0300

    adiciona texto do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
-import '../styles/header.scss'
+import "../styles/header.scss";
 
 export function Header() {
   return (
     <header className="header">
       <div>
-        <img src="/logo.svg" alt="to.do"/>
+        <p>
+          to.<strong>do</strong>
+        </p>
       </div>
     </header>
-  )
+  );
 }
